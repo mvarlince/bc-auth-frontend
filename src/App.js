2fb71e9dcdbb2e35fb36dff6b38dda35e25dbbab
Todd Albert <todd@bocacode.com>
2023-02-09 10:25:51 -0500

    added context and signup form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, createContext } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Welcome from './scenes/Welcome'
 import Login from './scenes/Login'
@@ -6,21 +6,25 @@ import Signup from './scenes/Signup'
 import Content from './scenes/Content'
 import './App.css';
 
+export const AuthContext = createContext(null);
+
 function App() {
   const [user, setUser] = useState();
   return (
-    <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <Routes>
-            <Route path="/secret" element={(user) ? <Content /> : <Welcome />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route exact path="/" element={<Welcome />} />
-          </Routes>
-        </header>
-      </div>
-    </BrowserRouter>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <Routes>
+              <Route path="/secret" element={(user) ? <Content /> : <Welcome />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route exact path="/" element={<Welcome />} />
+            </Routes>
+          </header>
+        </div>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,3 +1,33 @@
+import { useState, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../App'
+
 export default function SignupForm() {
-  return <h2>Signup Form</h2>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { setUser } = useContext(AuthContext)
+  const navigate = useNavigate()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // make a post request to the API with the form data
+    // create a new user in the database
+    // then...
+    // 1. do something with the new user
+    setUser({ email, password })
+    // 2. redirect to the content page
+    navigate('/secret')
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email &nbsp;
+        <input type="email" name="email"
+          value={email} onChange={e => setEmail(e.target.value)} />
+      </label><br />
+      <label htmlFor="password">Password &nbsp;
+        <input type="password" name="password"
+          value={password} onChange={e => setPassword(e.target.value)} />
+      </label><br />
+      <input type="submit" value="Sign Up" />
+    </form>
+  )
 }
